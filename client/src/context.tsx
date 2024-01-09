304d496241f6de5c3da73b6b7a13725bbe5ddd86
Aayush Gupta <cdtaayushgupta@gmail.com>
2024-01-10 03:48:33 +0530

    added pages in context

--- client/src/context.tsx
+++ client/src/context.tsx
@@ -3,18 +3,23 @@ import { ReactNode, createContext, useState } from 'react';
 interface ContextData {
     name: string,
     setName: (newName: string) => void,
+    currPage: number,
+    setCurrPage: (newName: number) => void,
 }
 
 const context = createContext<ContextData>({ 
     name: "",
     setName: () => {},
+    currPage: 0,
+    setCurrPage: () => [],
 });
 
 const ContextProvider = ({ children }: { children: ReactNode }) => {
     
     const [name, setName] = useState<string>("");
+    const [currPage, setCurrPage] = useState<number>(0);
   
-    const contextValue = { name, setName };
+    const contextValue = { name, setName, currPage, setCurrPage };
   
     return (
         <context.Provider value={contextValue}>
