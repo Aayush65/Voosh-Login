162120678a891b8f24fb3024e409e33dda3aea3f
Aayush Gupta <cdtaayushgupta@gmail.com>
2024-01-09 17:47:34 +0530

    Adding Views

---
+++ server/src/views/view.ts
@@ -0,0 +1,34 @@
+import { Response } from 'express'; 
+
+export function statusOkay(res: Response, message: any) {
+    res.status(200).json(message);
+}
+
+export function created(res: Response, message: any) {
+    res.status(201).json(message);
+}
+
+export function statusOkayNoMsg(res: Response, message: any) {
+    res.status(200).json(message);
+}
+
+export function badRequest(res: Response) {
+    res.status(400).json({message: 'Bad Request'});
+}
+
+export function unauthAccess(res: Response) {
+    res.status(401).json({message: 'Unauthorised'});
+}
+
+export function wrongCredentials(res: Response) {
+    res.status(403).json({message: 'Forbidden'});
+}
+
+export function notFound(res: Response) {
+    res.status(404).json({message: 'Not Found'});
+}
+
+export function serverError(res: Response, err: unknown) {
+    console.log(err);
+    res.status(500).json({message: 'Internal Server Error'});
+}
