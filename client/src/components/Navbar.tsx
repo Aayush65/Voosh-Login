f70d2ed47d7e02740012a7a3bc6b2f667e723c46
Aayush Gupta <cdtaayushgupta@gmail.com>
2024-01-10 03:49:05 +0530

    Added page changing functionality to navbar

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -5,7 +5,7 @@ import { useNavigate } from "react-router-dom";
 
 
 const Navbar = () => {
-    const { name, setName } = useContext(context);
+    const { name, setName, currPage, setCurrPage } = useContext(context);
     const navigate = useNavigate();
 
     function logout() {
@@ -14,10 +14,15 @@ const Navbar = () => {
         navigate("/login");
     }
 
+    const pages : {[key: number]: string} = {0: "Your Orders", 1: "New Order?"}
+
     return (
-        <div className="w-full flex justify-between py-5 px-10 border-b-2 border-slate-300">
-            <h1 className=" font-black text-3xl tracking-wide">Welcome, {name.split(' ')[0]}</h1>
-            <img src={logoutIcon} alt="logout" className="w-10 h-10 hover:scale-110 cursor-pointer" onClick={() => logout()} />
+        <div className="w-full flex justify-between py-5 px-10 border-b-2 border-slate-300 sticky">
+            <h1 className="font-black text-3xl tracking-wide flex items-center">Welcome, {name.split(' ')[0]}</h1>
+            <div className="flex gap-10">
+                <h1 className="text-2xl font-semibold flex items-center hover:underline cursor-pointer" onClick={() => setCurrPage(1 - currPage)}>{pages[1 - currPage]}</h1>
+                <img src={logoutIcon} alt="logout" className="w-10 h-10 hover:scale-110 cursor-pointer" onClick={() => logout()} />
+            </div>
         </div>
     )
 }
