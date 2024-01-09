f713dc659ede95b856b32a529f374381c54b57b8
Aayush Gupta <cdtaayushgupta@gmail.com>
2024-01-10 02:11:16 +0530

    Added routes & check for server health

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,48 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import MainSite from "./MainSite";
+import { Login, Register, ServerError, NonExistant, FirstLoad } from "./pages";
+import { useState, useEffect } from "react";
+import { siteLoc } from "./constants";
 
 function App() {
+	const [ isServerActive, setIsServerActive ] = useState(false);
+	const [ isTimeout, setIsTimeout ] = useState(false);
 
-  return (
-    <div className="w-20 h-20 bg-red-600">
-    </div>
-  )
+	useEffect(() => {
+		async function ping() {
+			try {
+				await fetch(siteLoc, { method: 'get' });
+				setIsServerActive(true);
+				return true;
+			} catch (error) {
+				setIsServerActive(false);
+				return false;
+			}	
+		}
+
+		ping();
+		let i = 0;
+		const id = setInterval(async () => {
+			const res = await ping();
+			if (res || i > 10)
+				clearInterval(id);
+			if (i > 10)
+				setIsTimeout(true);
+			i ++;
+		}, 500);
+		ping();
+	}, [])
+
+	return isServerActive ? (
+		<Router>
+			<Routes>
+				<Route path="/" element={<MainSite />} />
+				<Route path="/login" element={<Login />} />
+				<Route path="/register" element={<Register />} />
+				<Route path="/*" element={<NonExistant />} />
+			</Routes>
+		</Router>
+	) : isTimeout ? <ServerError /> : <FirstLoad />
 }
 
 export default App
