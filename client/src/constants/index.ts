1f6888741d753a0b7f06a9c424bbc98d2d9b8508
Aayush Gupta <cdtaayushgupta@gmail.com>
2024-01-10 13:05:06 +0530

    Changing site address to render

--- client/src/constants/index.ts
+++ client/src/constants/index.ts
@@ -1,3 +1,4 @@
-export const siteLoc = "http://localhost:3000";
+// export const siteLoc = "http://localhost:3000";
+export const siteLoc = "https://voosh-server-9mpk.onrender.com";
 
 export const firstLoadComment = "The first loading may take some time due to free hosting";
